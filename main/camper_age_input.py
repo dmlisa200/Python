83d1c13a55214f8db45d1e1aef01272384a916c7
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-03 20:11:21 -0500

    Add files via upload
    Python test of convert_to_months with input of 12

---
+++ main/camper_age_input.py
@@ -0,0 +1,6 @@
+def convert_to_months(x):
+    return x/12
+
+
+if__name__=='__main__':
+    convert_to_months(x)
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,10 +1,9 @@
 import unittest
 from main import camper_age_input
 
-
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_convert_to_months(self):
+        self.assertEqual(1, main.convert_to_months(12))
 
 
 if __name__ == '__main__':
